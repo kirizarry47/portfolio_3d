16fa2090d72a07cabf627de6cd1bf30c3fb42758
kirizarry47 <68522693+kirizarry47@users.noreply.github.com>
2023-03-08 15:15:21 -0500

    layout complete nav bar started

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useEffect,useState } from "react";
+import React from "react";
+import {Link } from "react-router-dom";
+
+
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu,close } from "../assets";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled]= useState(false);
+
+  useEffect(() => {
+    const handleScroll = () =>{
+      const scrollTop =window.scrollY;
+      if(scrollTop >100) {
+        setScrolled(true);
+      }  else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  
+} 
 
 export default Navbar
